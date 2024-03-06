f1f93917c144c0a9a47494593fc9572c576b36d6
vilmajudin <vilma.judin@aalto.fi>
2024-03-06 17:30:07 +0200

    Käyttöliittymää työstetty. Siihen voi nyt lisätä uuden kuvan, joka skaalataan oikeaan kokoon. Ei käsittele virhetilanteita, ei muita toimintoja

--- src/main/scala/DesingGUI.scala
+++ src/main/scala/DesingGUI.scala
@@ -1,8 +1,14 @@
 import scalafx.application.JFXApp3
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.{HBox, Pane, VBox}
+import scalafx.scene.control.{Button, Label, TextField}
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.layout.{Background, ColumnConstraints, GridPane, HBox, Pane, RowConstraints, VBox}
 import scalafx.scene.shape.Rectangle
 import scalafx.scene.paint.Color.*
+import scalafx.stage.Popup
+
+import java.io.FileInputStream
 
 /** Luokan tynkä, jota on tarkoitus muokata. **/
 
@@ -15,10 +21,10 @@ object DesingGUI extends JFXApp3:
       width = 600
       height = 450
 
-    val root = HBox()
+    val root = VBox()
 
-    val scene = Scene(parent = root)
-    stage.scene = scene
+    val scene1 = Scene(parent = root)
+    stage.scene = scene1
 
     val sidePanel = new VBox()
 
@@ -26,25 +32,57 @@ object DesingGUI extends JFXApp3:
       x = 275
       y = 175
       width = stage.width.toDouble / 4
-      height = stage.height.toDouble / 2
+      height = (stage.height.toDouble-70) / 2
       fill = Red
 
     val rectangle3 = new Rectangle:
       x = 275
       y = 175
       width = stage.width.toDouble / 4
-      height = stage.height.toDouble / 2
+      height = (stage.height.toDouble-70) / 2
       fill = Green
 
-    val mainScene = new Rectangle:
-      x = 275
-      y = 175
-      width = stage.width.toDouble * 3/4
-      height = stage.height.toDouble
-      fill = Blue
+    var mainScene = new ImageView(Image(FileInputStream("/Users/vilmajudin/Desktop/Koulu hommat/Vuosi 1/Periodi 3/MagicOfInteriorDesign/src/test/piirrustus.jpeg"))):
+      fitHeight = stage.height.toDouble - 70
+      fitWidth = stage.width.toDouble * 3/4
+
+    /**Luo uuden ikkunan, jonne lisätään kuvatiedoston polku**/
+
+
+    val d = new HBox():
+      children = Array(mainScene, sidePanel)
+
+
+    val popUp = new VBox():
+      val label = Label("Add the path to your floorplan")
+      val text1 = TextField()
+      val button = new Button("Submit"):
+        onAction = (event) =>
+          mainScene = new ImageView(Image(FileInputStream(text1.text.value))):
+                  fitHeight = stage.height.toDouble - 70
+                  fitWidth = stage.width.toDouble * 3/4
+          stage.scene = scene1
+          d.children.clear()
+          d.children = Array( mainScene, sidePanel)
+          text1.clear()
+      text1.promptText = "Add the path to your floorplan here"
+      children = Array( label, text1, button )
+
+    val scene2 = Scene(parent = popUp)
+
+    val addButton = new Button( "Add floorplan"):
+      onAction = (event) => stage.scene = scene2
+
+    val bottomBar = new HBox():
+      padding = Insets.apply(10, 10, 10, 10)
+      spacing = 10
+
+      background = Background.fill(White)
+      children = Array(addButton)
 
     sidePanel.children = Array(rectangle2, rectangle3)
-    root.children = Array(mainScene, sidePanel)
+    root.children = Array(d, bottomBar)
+
 
   end start
 
